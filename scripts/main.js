6c1de4ccf65c7422449511895d5dceb8d36032f0
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2023-12-29 13:24:17 -0500

    modified s.css and main.js filed

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,17 @@
 // containers
 const app = document.querySelector("#app");
 
-// STEP #1: function to create function to create an element
-// and can add a class if that element needs one
-function createElement(tag, className) {
-  const elmnt = document.createElement(tag);
-  className ? elmnt.classList.add(className) : tag;
+// STEP #3:
+function getNotes() {
+  return JSON.parse(localStorage.getItem("stickyNotes") || "[]");
+}
 
-  return elmnt;
+function saveNotes(notes) {
+  localStorage.getItem("stickyNotes", JSON.stringify(notes));
+}
+
+function addNote() {
+  const getNotes = getNotes();
 }
 
 // STEP #2: create elements  and append where it is necessary
@@ -17,7 +21,7 @@ function appContent() {
   const div = createElement("div", "app-content");
 
   div.appendChild(stickyHead());
-  div.appendChild(textArea());
+  div.appendChild(createTextArea());
   div.appendChild(stickyBottom());
 
   return div;
@@ -46,13 +50,32 @@ function stickyBottom() {
   return section;
 }
 
+/**
+ *
+ *
+ *
+ */
 function addStickyNote() {
   const plus = createElement("button", "sticky-btn");
   plus.classList.add("plus");
   plus.textContent = "+";
 
+  plus.addEventListener("click", () => {
+    // append app content to the app
+    app.appendChild(appContent());
+
+    // return app content
+    return appContent();
+  });
+
   return plus;
 }
+
+/**
+ *
+ *
+ *
+ */
 
 function dotStickyNote() {
   const dots = createElement("button", "sticky-btn");
@@ -70,11 +93,11 @@ function closeStickyNote() {
   return close;
 }
 
-function textArea(id, content) {
+function createTextArea(id, content) {
   const txtAr = createElement("textarea", "notes");
-  //   txtAr.value = content;
-  txtAr.placeHolder = "Take note...";
-  txtAr.value = txtAr.placeHolder;
+  txtAr.placeholder = "Take note...";
+  content = txtAr.placeholder;
+  txtAr.value = content;
 
   return txtAr;
 }
@@ -109,6 +132,15 @@ function strikethrough() {
   btn.textContent = "ab";
 
   return btn;
+}
+
+// STEP #1: function to create function to create an element
+// and can add a class if that element needs one
+function createElement(tag, className) {
+  const elmnt = document.createElement(tag);
+  className ? elmnt.classList.add(className) : tag;
+
+  return elmnt;
 }
 
 app.appendChild(appContent());
